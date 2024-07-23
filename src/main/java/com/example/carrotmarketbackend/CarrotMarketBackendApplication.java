ae1a03262815cf94ac6d0fc9252c9dd4929fed2e
유종화 <sola201@naver.com>
2024-07-23 23:27:17 +0900

    Initial commit

---
+++ src/main/java/com/example/carrotmarketbackend/CarrotMarketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrotmarketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotMarketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrotMarketBackendApplication.class, args);
+    }
+
+}
